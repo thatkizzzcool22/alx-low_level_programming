6aa7e9b0f253681ee76ee34fc6115251f2df4a9e
thatkizzzcool22 <Ishiaqokizzz@gmail.com>
2022-12-13 07:21:59 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char i, c;
-
-	for (i = 0; i <= 9; i++)
+	char c, i;
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		for (i = 0; i <= 9; i++)
 		{
-			_putchar(c);
+			for (c = 'a'; c <= 'z'; c++)
+			{
+				_putchar(c);
+			}
 		}
 	}
-	_putchar('\n');
+	_putchar(c);
 }
