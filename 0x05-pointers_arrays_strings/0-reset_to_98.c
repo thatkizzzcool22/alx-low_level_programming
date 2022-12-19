f97de3069f530dd46c15aebca44cff717c96c8dd
thatkizzzcool22 <Ishiaqokizzz@gmail.com>
2022-12-19 05:51:29 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * @n: int to check
+ *
+ * Return: 0 is success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+am coding 
+and am now doing the task on pointers
