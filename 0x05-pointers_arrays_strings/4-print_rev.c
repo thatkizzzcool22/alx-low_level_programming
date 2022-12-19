e8f8f252d497495560a88f18e0fb4553be617ffb
thatkizzzcool22 <Ishiaqokizzz@gmail.com>
2022-12-19 06:59:54 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - that prints a string, in reverse, followed by a new line
+ * @s: the spring to be printedin rev
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
