65092f8182a0201b512f4fe6fc25c35a3fc98d35
thatkizzzcool22 <Ishiaqokizzz@gmail.com>
2022-12-19 06:44:45 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: the spring
+ *
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
