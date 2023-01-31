5c2a2ff80255c86defaec29cee19d07f243f0fec
thatkizzzcool22 <Ishiaqokizzz@gmail.com>
2023-01-31 05:48:17 -0800

    all tasks

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of a elements in a linked listint_list
+ * @n: printer in the linker list
+ *
+ * Return: number of the elements in a linked listint_t list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+Am trying to get my code and going to make sure i get it
+though it is not easy
