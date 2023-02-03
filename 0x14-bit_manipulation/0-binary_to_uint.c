f3a920b9be8cb0749618e23526a7e81bfb95ca8c
thatkizzzcool22 <Ishiaqokizzz@gmail.com>
2023-02-03 06:30:21 -0800

    All tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,51 +1,34 @@
+
 #include "main.h"
 
 /**
- * power - a function that return a power of a number
- *
- * @a: an integer Base
- * @b: an integer exponent
- *
- * Return: the a ^ b
- *
+ * binary_to_uint- convert binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
  */
-int power(int a, int b)
-{
-	if (b == 0)
-		return (1);
-	else
-		return (power(a, b - 1) * a);
-}
-
-/**
- * binary_to_uint - convert binary ot decimal
- *
- * @b: a string that contain binary number
- *
- * Return: a number,
- * 0 if b is null
- * 0 if b contain any lettre that is not 0 or 1
- *
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int i, j, k;
-	int count = 0;
 
-	/* print thr count of the string b */
-	for (j = 0; b[j] != '\0'; j++)
-		count++;
-	/* printf("count = %d\n", count); */
-	for (i = count - 1, k = 0; i >= 0; i--, k++)
+	int night = 0, j;
+	unsigned int number = 0, non_binary = 0;
+
+	if (b == NULL)
+		return (non_binary);
+
+	while (b[night] != '\0')
+		night++;
+	night -= 1;
+	j = 0;
+	while (b[j])
 	{
-		/* printf("d[%d] = %d\n", i, b[i]); */
-		if (!b[i] || (b[i] != 48 && b[i] != 49))
-			return (0);
-		else if (b[i] == 49)
-			/* printf("%s = %d\n", b, b[i]);*/
-			decimal += power(2, k);
+		if ((b[j] != '0') && (b[j] != '1'))/*&& logical AND operator*/
+			return (non_binary);
+
+		if (b[j] == '1')
+			number += (1 * (1 << night));/*<< bitwise left shift operator*/
+		j++;
+
+		night--;
 	}
-	return (decimal);
+	return (number);
 }
